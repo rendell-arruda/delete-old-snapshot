5018bc194d2213570ca0e35ffa18a3bb0317dc9a
Rendell Arruda <rendellarruda3@gmail.com>
2024-08-11 17:19:39 -0300

    feat: add startt/Users/rendellarruda/.pyenv/versions/3.12.2/bin/python /Users/rendellarruda/Desenvolvimento/delete-old-snapshot/main.py

--- main.py
+++ main.py
@@ -11,17 +11,32 @@ retention_days = 30
 regions = ['us-east-1']
     
 def lambda_handler(retetion_days,region):
+    # O propósito dessa linha é calcular a data limite (ou seja, retention_date) 
+    # a partir da qual você deseja excluir snapshots.
+    #                retorna a data atual       -  timedelta cria um intervalo de tempo de 30 dias 
     retention_date = datetime.now(timezone.utc) - timedelta(days=retention_days)
 
+    print("data de retenção")
     print(retention_date)
     
     client = session.client('ec2', region_name=region)
     try:
         # Lista de snapshots
         snapshots = client.describe_snapshots(OwnerIds=['self'])["Snapshots"]
-        for snapshot in snapshots:            
-            snapshot_date = snapshot['StartTime']
-            print(snapshot_date)    
+        for snapshot in snapshots:
+            print("========= separador =========")
+            # print(f"O snaphot : {snapshot["Description"]}, foi criado em: {snapshot["StartTime"]}")
+            snapshot_date = snapshot["StartTime"]
+            # se a data do snapshot for menor que a data de retenção, quer dizer que ele é mais antigo que a data de retenção
+            if snapshot_date < retention_date:
+                snapshot_id = snapshot['SnapshotId']
+                
+                ...
+            else:
+                days_rest = snapshot_date - retention_date
+                print(f"Faltam {days_rest} para o snapshot ser deletado")
+         
+           
     except Exception as e:
         return print(e)
 
